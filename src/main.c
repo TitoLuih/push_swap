6fb8d13b1bbf2ed01eaf35cb5795ed2c7c68d6eb
Luis Ruiz Torres <lruiz-to@c3r9s5.42malaga.com>
2025-03-13 15:59:27 +0100

    first push

---
+++ src/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lruiz-to <lruiz-to@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 15:27:20 by lruiz-to          #+#    #+#             */
+/*   Updated: 2025/03/13 15:51:35 by lruiz-to         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char *argv)
+{
+	if(argc == 2)
+		//funcion que use split para dividir los numeros
+	else
+	{
+		//funcion que detecte si son char * o int y los pase a enteros
+	}	
+}
---
+++ src/push_swap.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lruiz-to <lruiz-to@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 15:29:15 by lruiz-to          #+#    #+#             */
+/*   Updated: 2025/03/13 15:58:30 by lruiz-to         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "../libs/libft/libft.h"
+
+typedef struct p_s_stack
+{
+	int				*content;
+	struct s_list	*next;
+}	t_stack;
+
+typedef struct push_swap
+{
+	p_s_stack *a;
+	p_s_stack *b;
+};
+
+
+int	ft_error();
+
+#endif
